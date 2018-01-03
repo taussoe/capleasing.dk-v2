adb769d68586a1e870515bf822e0ce1e7befe9f3
Taus Soe <taus.soe@gmail.com>
2018-01-03 18:48:37 +0100

    gatsby

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,11 +1,14 @@
-const path = require('path');
+const path = require('path')
 
 exports.createPages = ({ boundActionCreators, graphql }) => {
-  const { createPage } = boundActionCreators;
+  const { createPage } = boundActionCreators
 
   return graphql(`
     {
-      allMarkdownRemark(sort: { order: DESC, fields: [frontmatter___date] }, limit: 1000) {
+      allMarkdownRemark(
+        sort: { order: DESC, fields: [frontmatter___date] }
+        limit: 1000
+      ) {
         edges {
           node {
             excerpt(pruneLength: 400)
@@ -65,15 +68,19 @@ exports.createPages = ({ boundActionCreators, graphql }) => {
     }
   `).then(result => {
     if (result.errors) {
-      result.errors.forEach(e => console.error(e.toString()));
-      return Promise.reject(result.errors);
+      result.errors.forEach(e => console.error(e.toString()))
+      return Promise.reject(result.errors)
     }
     result.data.allMarkdownRemark.edges.forEach(({ node }) => {
-      createPage({
-        path: node.frontmatter.path,
-        component: path.resolve(`src/templates/${String(node.frontmatter.templateKey)}.js`),
-        context: {} // additional data can be passed via context
-      });
-    });
-  });
-};
+      if (node.frontmatter.templateKey) {
+        createPage({
+          path: node.frontmatter.path,
+          component: path.resolve(
+            `src/templates/${String(node.frontmatter.templateKey)}.js`
+          ),
+          context: {}, // additional data can be passed via context
+        })
+      }
+    })
+  })
+}
