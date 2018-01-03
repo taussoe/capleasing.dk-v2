4e985c2bbc1b39aff04dd2f4f5466b709f0314c6
Taus Soe <taus.soe@gmail.com>
2018-01-03 17:10:49 +0100

    contact

--- src/components/kontakt.js
+++ src/components/kontakt.js
@@ -3,6 +3,7 @@ import styled from 'styled-components'
 
 const KontaktContainer = styled.div`
   background-color: #333333;
+  display: flex;
 `
 
 export default props => <KontaktContainer>Kontakt</KontaktContainer>
